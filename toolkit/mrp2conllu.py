8380ebddac5074be4042a231375e62d7eb31901b
ruixiangcui <cuirx@outlook.com>
2020-07-15 03:42:30 +0200

    support convert chinese udpipe without anchors

--- toolkit/mrp2conllu.py
+++ toolkit/mrp2conllu.py
@@ -4,16 +4,22 @@ import collections
 import argparse
 
 
-def ud2companion(ud):
+def mrp2conllu(ud):
     lines = []
     for node in ud["nodes"]:
-        if len(node["anchors"]) > 1:
+        if "anchors" in node and len(node["anchors"]) > 1:
             print("Multi-anchor in ud: {} \n (node: {})".format(ud, node["id"]))
         # id, token, lemma, upos, xpos, '_', head, label, '_', range
-        line = [str(node["id"]), node["label"], node["values"][0], node["values"][1],
-                node["values"][2], '_', '_', '_', '_', 'TokenRange=' + str(node["anchors"][0]["from"])
-                + ':' + str(node["anchors"][0]["to"])]
+        #zho.mrp does not have anchors
+        if "anchors" in node:
+            line = [str(node["id"]), node["label"], node["values"][0], node["values"][1],
+                    node["values"][2], '_', '_', '_', '_', 'TokenRange=' + str(node["anchors"][0]["from"])
+                    + ':' + str(node["anchors"][0]["to"])]
+        else:
+            line = [str(node["id"]), node["label"], node["values"][0], node["values"][1],
+                    node["values"][2], '_', '_', '_', '_']
         lines.append(line)
+
     # print (lines)
     if "edges" in ud:
         for edge in ud["edges"]:
@@ -31,24 +37,24 @@ def ud2companion(ud):
 
 
 
-parser = argparse.ArgumentParser(description="Preprocess evaluation files");
-parser.add_argument("udpipe", type=str, help="udpipe input file");
+parser = argparse.ArgumentParser(description="Convert udpipe mrp to conllu");
+parser.add_argument("input", type=str, help="udpipe mrp input file");
 parser.add_argument("--outdir", type=str, help="output dir");
 args = parser.parse_args();
 
 if args.outdir is not None:
-    file = args.outdir + 'udpipe.conllu'
-    with open(args.udpipe, 'r') as fi_ud, open(file, 'wb') as fo:
+    file = args.outdir  + str(args.input).split("/")[-1].strip(".mrp") + '.conllu'
+    with open(args.input, 'r') as fi_ud, open(file, 'wb') as fo:
         line_ud = fi_ud.readline().strip()
         while line_ud:
             ud = json.loads(line_ud, object_pairs_hook=collections.OrderedDict)
-            conllu =  ud2companion(ud)
+            conllu =  mrp2conllu(ud)
             line_ud = fi_ud.readline().strip()
             for i in conllu:
                 item = "    ".join(i) if type(i) == list else i
                 # item = item.replace("\"", "")
-                fo.write((json.dumps(item) + '\n').replace("\"", "").encode('utf-8'))
-            fo.write("\n")
+                fo.write((json.dumps(item) + '\n').replace("\"", "").encode())
+            fo.write(("\n").encode())
 
 print("Conversion finished.")
 
